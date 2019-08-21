6be78262fd1d6705c09d07a3d790bad29f52afdf
macs-code <omambiadauglous@gmail.com>
2019-08-21 10:43:23 +0300

    [chore] add registration, login and profile resources

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Require Authentication
+Route::middleware('auth:api')->group(function(){
+    Route::get('/user-profile', 'API\AuthController@profile');
 });
