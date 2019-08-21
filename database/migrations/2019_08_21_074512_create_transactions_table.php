c53c281fcc99fcb192ba15c26b4d81293281e280
macs-code <omambiadauglous@gmail.com>
2019-08-21 11:12:28 +0300

    [chore] add necessary models and migrations
    added category, product and transaction  models and migrations

---
+++ database/migrations/2019_08_21_074512_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('product_id');
+            $table->integer('user_id');
+            $table->double('amount');
+            $table->integer('quantity');
+            $table->timestamps();
+
+
+            // $table->foreign('product_id')->references('id')->on('products');
+            // $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
