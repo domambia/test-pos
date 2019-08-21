7a13b21e00f7e88d331be376da90b42490ee092f
macs-code <omambiadauglous@gmail.com>
2019-08-21 11:11:14 +0300

    [chore] add important models and migrations
    Added product, category, and transcation models and migrations

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Transaction;
 
 class User extends Authenticatable
 {
@@ -37,4 +38,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
 }
